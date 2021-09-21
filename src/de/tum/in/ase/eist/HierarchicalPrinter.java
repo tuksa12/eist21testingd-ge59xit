acb825809f92068d57c143163166f56f6ed542bd
Christian Femers <femers@in.tum.de>
2021-09-21 20:30:55 +0200

    Add Javadoc

--- src/de/tum/in/ase/eist/HierarchicalPrinter.java
+++ src/de/tum/in/ase/eist/HierarchicalPrinter.java
@@ -2,5 +2,12 @@ package de.tum.in.ase.eist;
 
 public interface HierarchicalPrinter {
 
+    /**
+     * Prints the given name with the correct formatting for the given level.
+     *
+     * @return <code>true</code> if the printing was successful and more lines can
+     *         be printed or <code>false</code> if the printing failed, e.g. if the
+     *         process was cancelled by the user
+     */
     boolean print(String name, int level);
 }
